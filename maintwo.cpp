e1ede0bb96e6a8b2a5b1375b79c2104fd138dfae
derpen <rizqyassabil@hotmail.com>
2024-04-08 13:38:52 +0700

    Camera zoom and mouse git add *

--- maintwo.cpp
+++ maintwo.cpp
@@ -12,6 +12,8 @@
 #include <glm/gtc/type_ptr.hpp>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);  
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
 void processInput(GLFWwindow *window);
 
 // settings
@@ -26,6 +28,14 @@ glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
 // The time
 float deltaTime = 0.0f; // Time between current frame and last frame
 float lastFrame = 0.0f; // Time of last frame
+
+// Camera mouse control
+bool firstMouse = true;
+float lastX = SCR_WIDTH / 2.0f;
+float lastY = SCR_HEIGHT / 2.0f;
+float yaw = -90.0f;
+float pitch = 0.0f;
+float fov = 45.0f;
 
 int main() {
 
@@ -52,8 +62,12 @@ int main() {
 	}    
 
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);  
+	glfwSetCursorPosCallback(window, mouse_callback);
+	glfwSetScrollCallback(window, scroll_callback);
 
 	glEnable(GL_DEPTH_TEST);
+
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 
 	// Compiling Shaders from our own class
 	Shader myShader("simpleVertexShader.vert", "simpleFragmentShader.frag");
@@ -196,11 +210,6 @@ int main() {
 	glUniform1i(glGetUniformLocation(myShader.ID, "bocchiFace"), 0);
 	glUniform1i(glGetUniformLocation(myShader.ID, "osakaFace"), 1);
 
-	// We do this outside now
-	glm::mat4 projection = glm::mat4(1.0f);
-	projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
-	myShader.setMat4("projection", projection);
-
 	while(!glfwWindowShouldClose(window))
 	{
 		// Handle time
@@ -208,7 +217,7 @@ int main() {
 		deltaTime = currentFrame - lastFrame;
 		lastFrame = currentFrame;
 
-		std::cout << "Delta time is: " << deltaTime << " LastFrame is: " << lastFrame << "\n" << std::endl;
+		// std::cout << "Delta time is: " << deltaTime << " LastFrame is: " << lastFrame << "\n" << std::endl;
 
 		processInput(window);
 
@@ -230,6 +239,9 @@ int main() {
 
 		//myShader.setMat4("projection", projection);
 		//myShader.setMat4("view", view);
+
+		glm::mat4 projection = glm::perspective(glm::radians(fov), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+		myShader.setMat4("projection", projection);
 
 		// camera/view transformation
 		glm::mat4 view = glm::lookAt(
@@ -281,4 +293,53 @@ void processInput(GLFWwindow *window)
 		cameraPos -= glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
 	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
 		cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+}
+
+void mouse_callback(GLFWwindow* window, double xpos, double ypos) {
+	if (firstMouse) {
+		lastX = xpos;
+		lastY = ypos;
+		firstMouse = false;
+	}
+
+	float xoffset = xpos - lastX;
+	float yoffset = lastY - ypos;
+	lastX = xpos;
+	lastY = ypos;
+
+	float sensitivity = 0.1f;
+	xoffset *= sensitivity;
+	yoffset *= sensitivity;
+
+	yaw += xoffset;
+	pitch += yoffset;
+
+	if (pitch > 89.0f) {
+		pitch = 89.0f;
+	}
+	if (pitch < -89.0f) {
+		pitch = -89.0f;
+	}
+
+	glm::vec3 direction;
+	direction.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
+	direction.y = sin(glm::radians(pitch));
+	direction.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
+
+	cameraFront = glm::normalize(direction);
+}
+
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset) {
+
+	fov -= (float)yoffset;
+
+	std::cout << "Fov is: " << fov << std::endl;
+
+	if (fov < 1.0f) {
+		fov = 1.0f;
+	}
+
+	if (fov > 45.0f) {
+		fov = 45.0f;
+	}
 }
